7bb511aff6758c9c9e83f7de032a40d398fb9ddd
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2013-10-29 09:28:09 +0100

    Forgot a code change thing
    Whoops.

--- index.php
+++ index.php
@@ -51,28 +51,28 @@
 	</head>
 
 	<body onload="JavaScript:init();">
-	<div class="title"><?php echo $party_name; ?></div>
-	<div class="contents" id="contents">
+		<div class="title"><?php echo $party_name; ?></div>
+		<div class="contents" id="contents">
+			<?php
+			                $query = "SELECT * FROM `" . $tbl_prefix.$tbl_slides . "` WHERE `published` LIKE 'yes';";
+			                $result = mysql_query($query);
+			                if(mysql_num_rows($result)==0||$result==false) { echo "Load error :("; } else {
+			                    echo '<h1>' . mysql_result($result, 0, "title") . '</h1>' . mysql_result($result, 0, "contents") . '<br /><br /><i>Posted by ' . mysql_result($result, 0, "poster") . mysql_result($result, 0, "Timeofpost") . '</i>'; }
+			?>
+		</div>
 		<?php
-		                $query = "SELECT * FROM `" . $tbl_prefix.$tbl_slides . "` WHERE `published` LIKE 'yes';";
+		                $query = "SELECT * FROM `" . $tbl_prefix.$tbl_addons . "`;";
 		                $result = mysql_query($query);
-		                if(mysql_num_rows($result)==0||$result==false) { echo "Load error :("; } else {
-		                    echo '<h1>' . mysql_result($result, 0, "title") . '</h1>' . mysql_result($result, 0, "contents") . '<br /><br /><i>Posted by ' . mysql_result($result, 0, "poster") . mysql_result($result, 0, "Timeofpost") . '</i>'; }
+		                $count = mysql_num_rows($result);
+		                for($i = 0; $i < mysql_num_rows($result); $i++)
+		                {
+		                    $pluginname = mysql_result($result, $i, "name");
+		                    echo '<div class="'.$pluginname.'" id="'.$pluginname.'">'."\r\n";
+		                    $url = mysql_result($result, $i, "url");
+		                    include "addons/" . $url;
+		                    echo "\r\n</div>\r\n";
+		                }
 		?>
-	</div>
-	<?php
-	                $query = "SELECT * FROM `" . $tbl_prefix.$tbl_addons . "`;";
-	                $result = mysql_query($query);
-	                $count = mysql_num_rows($result);
-	                for($i = 0; $i < mysql_num_rows($result); $i++)
-	                {
-	                    $pluginname = mysql_result($result, $i, "name");
-	                    echo '<div class="'.$pluginname.'" id="'.$pluginname.'">'."\r\n";
-	                    $url = mysql_result($result, $i, "url");
-	                    include "addons/" . $url;
-	                    echo "\r\n</div>\r\n";
-	                }
-	?>
 	</body>
 
 </html>
